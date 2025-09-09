3cd2e533761d4eca4d93e261ca1c581762ac6414
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-09 14:18:51 +0200

    add model loader for the GUI

---
+++ src/model_loader.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "model.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void load_model_from_path(const char* path);
+
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+Model* get_loaded_model();
+#endif
