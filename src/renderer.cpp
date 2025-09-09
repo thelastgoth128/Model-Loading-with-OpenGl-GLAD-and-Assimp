24f02da629d72cac35398118752681a61b63d5fc
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-09 14:18:02 +0200

    renderer implementation to load window

---
+++ src/renderer.cpp
@@ -0,0 +1,193 @@
+#include "renderer.hpp"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "shaders/shader.h"
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <cmath>
+#include "model.h"
+#include "model_loader.hpp"
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0,0,width,height);
+}
+
+glm::vec3 cameraPos =glm::vec3(0.0f, 0.0f, 5.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window,true);
+    }
+
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
+    const float cameraSpeed = 2.5f * deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        cameraPos += cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        cameraPos -= cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+}
+float lastX = 400, lastY = 300;
+float yaw, pitch;
+bool firstMouse = true;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos -lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    const float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if(pitch > 89.0f){
+        pitch = 89.0f;
+    }
+    if(pitch < -89.0f) {
+        pitch =-89.0f;
+    }
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+}
+float Zoom = 45.0f;
+void scrool_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    Zoom -= (float)yoffset;
+    if (Zoom < 1.0f)
+        Zoom = 1.0f;
+    if (Zoom > 45.0f)
+        Zoom = 45.0f;
+}
+
+void on_file_selected(const char* path) {
+    load_model_from_path(path);
+}
+
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+
+
+void launch_glfw_renderer() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window= glfwCreateWindow(800, 600,"Lighting",NULL,NULL);
+    if ( window == NULL) {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return;
+    }
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "Failed to initialize GLAD" << endl;
+    }
+
+    glViewport(0,0,1920,1080);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //Camera
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+    vec = trans * vec;
+
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+
+    glm::mat4 projection;
+    float fov = 45.0f;
+    projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    glm::mat4 view;
+    view = glm::lookAt( glm::vec3(0.0f, 0.0f, 3.0f),
+                        glm::vec3(0.0f, 0.0f, 0.0f),
+                        glm::vec3(0.0f, 1.0f, 0.0f));
+
+    Shader lightShader("C:/Users/HP/OneDrive/Documentos/Cyrus/Projects/OpenGL/src/shaders/vertex.vs","C:/Users/HP/OneDrive/Documentos/Cyrus/Projects/OpenGL/src/shaders/fragment.fss");
+    Shader lightSourceShader("C:\\Users/HP/OneDrive/Documentos/Cyrus/Projects/OpenGL/src/shaders/cubevertex.vs","C:/Users/HP/OneDrive/Documentos/Cyrus/Projects/OpenGL/src/shaders/cubefragment.fss");
+
+    Shader ourShader("C:/Users/HP/OneDrive/Documentos/Cyrus/Projects/OpenGL/src/shaders/Avertex.vs","C:/Users/HP/OneDrive/Documentos/Cyrus/Projects/OpenGL/src/shaders/Afragment.fss");
+
+    Model ourModel("C:/Users/HP/OneDrive/Documentos/Cyrus/Projects/model_loading/src/Textures/backpack.obj");
+//     if (!ourModel) {
+//     std::cerr << "No model loaded — aborting render.\n";
+//     return;
+// }
+
+std::cout << "[Renderer] Launching GLFW window...\n";
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        glfwSetCursorPosCallback(window, mouse_callback);
+        glfwSetScrollCallback(window, scrool_callback);
+
+        glEnable(GL_DEPTH_TEST);
+
+        glClearColor(0.0f, 0.0f, 0.0f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        const float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        glm::mat4 view;
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        ourShader.use();
+        ourShader.setInt("material.texture_diffuse1", 0);
+        ourModel.Draw(ourShader);
+
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "model");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        unsigned int transformLoc1 = glGetUniformLocation(ourShader.ID, "view");
+        glUniformMatrix4fv(transformLoc1, 1, GL_FALSE, glm::value_ptr(view));
+
+        glm::mat4 projection = glm::perspective(glm::radians(Zoom), 800.0f / 600.0f, 0.1f, 100.0f);
+        unsigned int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }   
+    
+    glfwTerminate();
+}
