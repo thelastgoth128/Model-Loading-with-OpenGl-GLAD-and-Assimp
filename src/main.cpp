5582a9f85c1df8864fa878c3231bdbc02c9b74a2
thelastgoth128 <thindwacyrus86@gmail.com>
2026-05-07 19:04:32 +0200

    added a default relative path to model

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-// #include "renderer.cpp"
+#include "renderer.cpp"
 
-// using namespace std;
+using namespace std;
 
-// int main() {
-//     launch_glfw_renderer();
-//     return 0;
-// }
+int main() {
+    launch_glfw_renderer("src//Textures//source//684c0f35-ba0b-48e0-ab19-db572ea748d3.glb");
+    return 0;
+}
